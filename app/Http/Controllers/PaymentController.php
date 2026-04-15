80aefdeff1d7fccced3fe29c1599f5b4d12f23be
almukhanbetov <almuko.m@gmail.com>
2026-04-15 18:38:59 +0500

    add  services.freedom.merchant_id');

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -17,7 +17,7 @@ class PaymentController extends Controller
         return view('payments.index', compact('user', 'sections', 'payments'));
     }
     public function init(Request $request)    {    
-        dd('INIT WORKS'); 
+        // dd('INIT WORKS'); 
         dd(config('services.freedom.merchant_id')); // 👈 ВСТАВЬ СЮДА         
         $user = Auth::user();
         $amount = $request->input('amount', 1000); // Сумма из модального окна
