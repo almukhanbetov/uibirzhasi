8ad6355e1e679422b31f8d26bf5314087e25be6a
almukhanbetov <almuko.m@gmail.com>
2026-04-16 17:54:35 +0500

    add view success

---
+++ resources/views/payments/success.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Оплата успешна</title>
+</head>
+<body style="text-align:center; padding:50px; font-family:sans-serif;">
+    <h1>✅ Оплата прошла успешно</h1>
+    <p>Баланс пополнен</p>
+
+    <a href="/payments">Вернуться назад</a>
+</body>
+</html>
