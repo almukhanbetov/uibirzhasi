aff756d6623b9db25804abdd6c3c05a0942f421e
almukhanbetov <almuko.m@gmail.com>
2026-06-29 19:18:12 +0500

    footer fix

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,4 +1,15 @@
 
+<style>
+    body {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+    .main {
+        flex: 1;
+    }
+</style>
+
 @include("includes.header")
 <main class="main">
     @yield('content')
